d91757d05f6c6208447a0edfd12fa7c91b05b7d3
shaquiesha davis <sdavis@chicagotechacademy.org>
2013-12-17 15:04:39 -0600

    initial commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :post_id, :text
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ActiveRecord::Base
-  attr_accessible :text, :title
+  has_many :comments
 end
